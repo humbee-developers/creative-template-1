9439a0a5c0431638a98f089bdd615b17eecad356
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-04-23 18:54:16 +0530

    theme -m

--- custom.js
+++ custom.js
@@ -241,4 +241,124 @@ jQuery(document).ready(function ($) {
       }
     }, speed);
   }
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  function initTimeline(){
+    let parent_container = document.getElementById("section-timeline");
+    let timeline_container = parent_container.querySelector(".timeline-container");
+    var sections = timeline_container.querySelectorAll(".year");
+  
+    const vh = (coef) => window.innerHeight * (coef/100);
+  
+    let parentST = ScrollTrigger.create({
+      id: "parent-timeline",
+      trigger: parent_container,
+      start:'top top',
+      toggleClass: 'started',
+      pin:true,
+      markers: true,
+      end: () => "+=" + ((sections.length - 1) * vh(50)),
+    });
+  
+    let currentSection;
+  
+    function goto(section, i) {
+      if (currentSection !== section) {
+        gsap.to(timeline_container, {
+          y: -50 * i,
+          duration: 0.6,
+          overwrite: true
+        });
+        let tl = gsap.timeline({defaults:{overwrite: true}});
+        if (currentSection) {
+          tl.to(currentSection.querySelector(".text-content h2"), {
+            fontSize: "2rem",
+          });
+          tl.to(currentSection, {
+            maxHeight: "3rem"
+          }, 0);
+          tl.to(currentSection.querySelectorAll(".text-content p"), {
+            opacity: 0,
+            duration: 0.25,
+            maxHeight: "0%"
+          }, 0);
+        }
+        currentSection = section;
+        if (section) {
+          tl.to(section.querySelector(".text-content h2"), {
+            fontSize: "5rem",
+          }, 0);
+          tl.to(section, {
+            maxHeight: "80vh"
+          }, 0);
+          tl.fromTo(section.querySelectorAll(".text-content p"), {maxHeight:"0%"}, {
+            opacity: 1,
+            maxHeight: "100%"
+          });
+        }
+      }
+    }
+  
+  
+    sections.forEach((sct, i) => {
+      let sct_index = sct.getAttribute('data-count');
+  
+      ScrollTrigger.create({
+        start: () => parentST.start + i * window.innerHeight * 0.4,
+        end: () => "+=" + window.innerHeight * 0.4,
+        markers: true,
+        onLeaveBack: () => i || goto(null, 0),
+        onToggle: self => self.isActive && goto(sct, sct_index)
+      });
+    });
+  
+  }
+  
+  initTimeline();
+  
+  var listItems = $(".timelineLIst .timeline_item");
+
+  // Create a GSAP timeline
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".timelineLIst",
+      start: "top center",
+      end: "bottom center",
+      scrub: true, // Smooth scrubbing effect
+      pin:true
+    }
+  });
+
+  // Add animations to the timeline
+  listItems.each(function(index, listItem) {
+    tl.to(listItem, {
+      color: "#002D70",
+      duration: 1,
+      ease: "none",
+      fontSize: "50",
+      onStart: function() {
+        // Remove highlight class from all list items except the current one
+        listItems.removeClass("highlight");
+        // Add highlight class to the current list item
+        $(listItem).addClass("highlight");
+        $(".listData").removeClass("highlightData");
+        $(listItem).next(".listData").addClass("highlightData");
+        listItems.css({
+          "font-size": "35px",
+          "color": "#A8A8A8",
+        });
+      },
+      onComplete: function() {
+        // Remove highlight class from the previous list item
+        if (index > 0) {
+          $(listItems[index - 1]).removeClass("highlight");
+          $(".listData").removeClass("highlightData");
+         
+        }
+      }
+    });
+
+    });
+
 });
